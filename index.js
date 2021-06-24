4f3df4283acc4323de1160adc2e873be74fdf135
SATVIK2204 <bksatvik@gmail.com>
2021-06-24 21:44:49 +0530

    Objects, Functions

--- index.js
+++ index.js
@@ -2,4 +2,31 @@ let name= 'Satvik';
 const InteresetRate=0.4;
 // InteresetRate=1; Error
 
-console.log(name);
+let age= 24;
+
+let isAdult= true;
+
+let isNull= null;
+
+let person={
+    name: 'Satvik',
+    age:21
+};
+
+// Dot notation
+person.age=24;
+person.name='Jhon';
+
+// Bracket Notation
+person['name']='Mary';
+
+function great(name){
+    person.name=name;
+    console.log(person)
+}
+great('Merry');
+
+function great(no){
+    return no*no;
+}
+console.log(great(2));
