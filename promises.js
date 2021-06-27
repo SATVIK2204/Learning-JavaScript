dc0511527be96d208639cd19be3f86867b2f4cff
SATVIK2204 <bksatvik@gmail.com>
2021-06-27 19:49:26 +0530

    Async JS | Promises

---
+++ promises.js
@@ -0,0 +1,43 @@
+const posts=[
+    { title: 'Post One', body: 'This is post one'},
+    { title: 'Post Two', body: 'This is post two'}
+];
+
+function getPosts(){
+    setTimeout(() => {
+        let output='';
+        posts.forEach((post, index) => {
+            output+=`<li>${post.title}</li>`;
+        });
+
+        document.body.innerHTML = output;
+    },1000);
+};
+
+function createPost(post){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            
+            const error= false;
+            if(!error){
+                resolve();
+            }
+            else{
+                reject('ERROR: Something went wrong');
+            }
+        }, 2000);
+    });
+}
+
+// createPost({ title: 'Post Three', body: 'This is post three'}).then(getPosts)
+// .catch(err => console.log(err));
+
+// Promise.all
+const promise1 =Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+setTimeout(resolve, 2000, 'Goodbye'));
+
+Promise.all([promise1, promise2, promise3]).then((values) => 
+console.log(values));
